67c45fd118c7d0814c10da73fbe059253e084b20
SueCide <misaj2007@gmail.com>
2026-01-22 18:09:52 +0100

    added red/green boarders to input fields, in register.js

--- Frontend/register.js
+++ Frontend/register.js
@@ -7,17 +7,23 @@ const password  = document.getElementById("Password");
 const password2 = document.getElementById("Password2");
 const button    = document.getElementById("confirm_button");
 const lbutton    = document.getElementById("login_button");
-const textvalue = document.createElement("div")
+const textvalue = document.createElement("div");
+const inputs = document.createElement("div");
 
 button.addEventListener("click",main) 
 
 let a = 1
 
+let name; 
+let pass1; 
+let pass2; 
+
 function main (){
 
-    const name = username.value;
-    const pass1 = password.value;
-    const pass2 = password2.value;
+    let name = username.value;
+    let pass1 = password.value;
+    let pass2 = password2.value;
+    
     
     if (pass1 !== pass2) {
         
@@ -69,6 +75,57 @@ function main (){
     }    
 }
 
+
+
+addEventListener("input", debounce(checkinput,1000))
+    
+function checkinput() {
+    
+    let name = username.value;
+    let pass1 = password.value;
+    let pass2 = password2.value;
+
+    if (pass1 !== pass2) {
+        
+        textvalue.textContent = `passwords doesn't match  [${a}]`
+        a++
+        text.appendChild(textvalue);
+        password2.style.borderColor = "red";
+        password.style.borderColor = "black";
+        username.style.borderColor = "black";
+
+    }else if(pass1==""||pass2==""||name==""){   
+        
+        textvalue.textContent = `please enter both username and password  [${a}]`
+        a++
+        text.appendChild(textvalue);
+        password2.style.borderColor = "red";
+        password.style.borderColor = "red";
+        username.style.borderColor = "red";
+
+    }else if(name.length<5 || pass1.length<5){
+
+        textvalue.textContent = `username and password has to be at least 5 symbols long  [${a}]`
+        a++
+        text.appendChild(textvalue);
+        password2.style.borderColor = "red";
+        password.style.borderColor = "red";
+        username.style.borderColor = "red";
+
+    }else{
+
+        password2.style.borderColor = "green";
+        password.style.borderColor = "green";
+        username.style.borderColor = "green";
+
+        textvalue.textContent = `everything's okay  [${a}]`
+        a++
+        text.appendChild(textvalue);
+    }
+
+}
+
+
 lbutton.addEventListener("click",login_b)
 
 function login_b() {
@@ -76,3 +133,18 @@ function login_b() {
     window.location.href = "login.html"
     
 }
+
+function wait(ms) {
+
+    return new Promise(resolve => setTimeout(resolve, ms));
+
+}
+
+function debounce(fn, delay) {
+    let timer;
+    return (...args) => {
+        clearTimeout(timer);
+        timer = setTimeout(() => fn(...args), delay);
+    };
+}
+
